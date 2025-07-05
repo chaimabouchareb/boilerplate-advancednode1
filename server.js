f37d82c91d1993ddccea1be6fa107f146dba1bf4
Chaima Bouchareb <chaimabouchareb17@gmail.com>
2025-07-05 01:23:32 +0000

    up to challenge 5

--- server.js
+++ server.js
@@ -1,23 +1,55 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const pug = require("pug");
 const app = express();
-app.set('view engine','pug');
-app.set('views','./views/pug');
+const cors = require("cors");
+app.use(cors());
+
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.route('/').get((req, res) => {
-  res.render('index');
-
+app.route("/").get((req, res) => {
+  res.render("index", { title: "Connected to Database", message: "Please log in" });
 });
 
+app.set("view engine", "pug");
+app.set("views", "./views/pug");
+
+let session = require("express-session");
+let passport = require("passport");
+let ObjectId = require('mongodb');
+let mongo = require('mongodb').MongoClient
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  });
+});
+let uri =
+  "mongodb+srv://xxxxxx:" +
+  process.env.PW +
+  "@xxxxxxxx.xxxxxxx.mongodb.net/xxxxxxxx?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
